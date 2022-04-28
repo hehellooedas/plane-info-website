af6215979d231dd8dcc9b29daaa8b286ce60f63e
maomao <2221362492@qq.com>
2022-04-28 20:02:45 +0800

    abcd

--- static/s-js/login.js
+++ static/s-js/login.js
@@ -1,8 +1,18 @@
 
 var login1btn=document.getElementById("login-btn");
+var username=document.getElementById('username');
+var password=document.getElementById('password');
+var obj={
+	email:'',
+	status:''
+};
+var databack;
+console.log(obj);
 var sendbtn=document.getElementsByClassName('send-btn')[0];
 var time=60;
-   sendbtn.addEventListener('click',function(){
+       sendbtn.addEventListener('click',function(){
+	   obj.email=username.value;
+	   console.log(obj);
 	   sendbtn.value="等待"+time+"s";
 	   sendbtn.className=".send-btn2";
 	   var ss=setInterval(function(){
@@ -15,13 +25,33 @@ var time=60;
 				clearInterval(ss);
 			}
 	   },1000);
+       $.ajax({
+		cache: true,
+		type: "POST",
+		url:'',
+		data:JSON.stringify(obj),//username=15194494354&password=aa  这样的
+		dataType:'json',
+		async: false,
+		error: function(request) {
+			alert("Connection error");
+		},
+		success: function(data) {
+            databack=data;
+		}
+	});
    })
    login1btn.addEventListener('click',function(){
+	obj.email=username.value;
+	obj.status=password.value;
+	if(obj.status===databack){
+		obj.status="true";
+	}
+	console.log(obj);
 	$.ajax({
 		cache: true,
 		type: "POST",
 		url:'',
-		data:$("#form-login").serialize(),//username=15194494354&password=aa  这样的
+		data:JSON.stringify(obj),//username=15194494354&password=aa  这样的
 		dataType:'json',
 		async: false,
 		error: function(request) {
