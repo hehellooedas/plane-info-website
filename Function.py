08e35c8520ec42cb4b50208292e81890041bcd08
April_Zhao <928309386@qq.com>
2022-04-30 05:27:28 +0800

    the 14th commit

--- Function.py
+++ Function.py
@@ -1,8 +1,27 @@
 import pandas,pickle,threading,multiprocessing,os,random
 from concurrent.futures import ThreadPoolExecutor,ProcessPoolExecutor
 
+
 def create_string(n=6):
     return ''.join(random.sample('abcdefghijklmnopqrstuvwxyz0123456789', n))
 
 def exist_account(account):
-    return False
+    with pandas.ExcelWriter('./files/emails.xlsx') as writer:
+        df = pandas.DataFrame(account)
+        df.to_excel(writer)
+
+
+def add_account(account):
+    emails = pandas.read_excel('./files/emails.xlsx',header=1)
+
+
+if __name__ == '__main__':
+    #df = pandas.DataFrame({'email':['928309386@qq.com']})
+    #df.to_excel('./files/emails.xlsx')
+    emails = pandas.read_excel('./files/emails.xlsx')
+    print(emails.head())
+    emails.loc[0] = ['928309386@qq.com']
+    #emails.to_excel('./files/emails.xlsx')
+    #d = {'email':'928309386@qq.com'}
+    #s = pandas.Series(d)
+
