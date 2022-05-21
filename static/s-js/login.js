a45ec1b82b6cd7d37fd155ca350fa3e2f1961852
maomao <2221362492@qq.com>
2022-05-21 17:36:51 +0800

    5216

--- static/s-js/login.js
+++ static/s-js/login.js
@@ -6,8 +6,8 @@ var sendbtn = document.getElementsByClassName('send-btn')[0];
 var databack;
 var time = 60;
 var information;
-var info=document.getElementsByClassName('info')[0];
-var codefail=document.getElementsByClassName('codefail')[0];
+var info = document.getElementsByClassName('info')[0];
+var codefail = document.getElementsByClassName('codefail')[0];
 sendbtn.addEventListener('click', function () {
 	if (!reg.test(use.value)) {
 		info.innerText = '输入格式错误';
@@ -15,13 +15,12 @@ sendbtn.addEventListener('click', function () {
 	else {
 		information = { 'email': email1.value };
 		$.ajax({
-
 			type: "POST",
 			url: '/login_ajax1',
 			data: information,
 			dataType: 'json',
 			async: false,
-			headers: {"X-CSRFToken":csrf_token},
+			headers: { "X-CSRFToken": csrf_token },
 			error: function (request) {
 				alert("Connection error");
 			},
@@ -33,6 +32,7 @@ sendbtn.addEventListener('click', function () {
 				else {
 					info.innerHTML = '<p>&nbsp</p>';
 					sendbtn.className = "send-btn2";
+					info.innerText = '邮件已发送,请注意查收';
 					var ss = setInterval(function () {
 						time--;
 						sendbtn.value = "等待" + time + "s";
@@ -49,31 +49,27 @@ sendbtn.addEventListener('click', function () {
 	}
 })
 // sendbtn.addEventListener('click', function () {
-	
 // });
 login1btn.addEventListener('click', function () {
 	if (databack == password.value) {
-	$.ajax({
+		$.ajax({
 
-		type: "POST",
-		url: '/login_ajax2',
-		data: information,
-		dataType: "text",
-		async: false,
-		headers: {"X-CSRFToken":csrf_token},
-		error: function (request) {
-			alert("Connection error");
-		},
-		success: function (data) {
-			location.replace(data);
-		}
-	});
+			type: "POST",
+			url: '/login_ajax2',
+			data: information,
+			dataType: "text",
+			async: false,
+			headers: { "X-CSRFToken": csrf_token },
+			error: function (request) {
+				alert("Connection error");
+			},
+			success: function (data) {
+				location.replace(data);
+			}
+		});
 	}
-	else { codefail.innerText='验证码输入错误，请重新输入'; }
+	else { codefail.innerText = '验证码输入错误，请重新输入'; }
 });
 //正则表达式；
-  var reg=/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
-  var use=document.getElementById('username');
-  use.addEventListener('blur',function(){
-	  
-  })
+var reg = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+var use = document.getElementById('username');
